c981ffc925ab106cc57094411e26a3f6a4a7400c
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2021-02-23 23:05:15 -0300

    Create remove.ts

---
+++ src/controllers/books/remove.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+import { bookModel } from "../../models/bookModel";
+
+export const remove = async (req: Request, res: Response) => {
+  const { _id } = req.params;
+
+  const book = await bookModel.findById(_id);
+
+  if (!book) return res.status(404).json({ error: `Book ID:${_id} not found` });
+
+  await bookModel.deleteOne({ _id });
+
+  return res
+    .status(204)
+    .json({ message: `Book ID:${_id} deleted successfully` });
+};
