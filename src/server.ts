6420135d135eb4e01ab9766dd8dce55f9c8825c1
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2021-02-27 04:12:41 -0300

    server use cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -7,6 +8,8 @@ const server = express();
 server.listen(3333);
 
 server.use(express.json());
+
+server.use(cors({ origin: true }));
 
 server.use(routes);
 
