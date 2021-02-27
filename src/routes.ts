04ab57f72cbb21ce917b89676b2f1e51e6d66f72
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2021-02-27 04:12:31 -0300

    getOne removed, getAllBooks already

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
 
-import {
-  create,
-  getAll,
-  getOne,
-  update,
-  remove,
-} from './controllers/books/index';
+import { create, getBooks, update, remove } from './controllers/books/index';
 
 const routes = Router();
 
 routes.post('/books', create);
-routes.get('/books', getAll);
-routes.get('/books/:tag', getOne);
+routes.get('/books/:tag?', getBooks);
 routes.put('/books/:_id', update);
-routes.delete('/books/:_id', remove);
+routes.delete('/book/:_id', remove);
 
 export default routes;
